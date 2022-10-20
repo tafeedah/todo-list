80730470a8531935db96537ed41da9a994e8ca98
Ahmad Khalil Jamil <coderkherleelaon@outlook.com>
2022-10-20 16:48:54 +0200

    #

--- app.js
+++
@@ -1,5 +0,0 @@
-var date = new Date();
-var current_date = date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDate();
-var current_time = date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
-var date_time = current_date+" "+current_time;
-document.getElementById("p1").innerHTML = date_time;
